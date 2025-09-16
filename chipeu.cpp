ffd60648880ce5fec73631e9f885ad4ac8516b2f
TheRogueVigilante <NONE>
2025-09-16 19:06:44 +0530

    Refactor chipeu.cpp

--- chipeu.cpp
+++ chipeu.cpp
@@ -1,71 +1,48 @@
 #include <fstream>
+#include <iostream>
+#include "chipeu.h"
 
 // Basic chip-8 system structure
-class chipeu {
-    public:
-        chipeu() : pc (0x200){
-            // Loading the fonstet in to the memory
-            for (int i = 0; i < 80; ++i){
-                memory[i] = chip8_fontset[i];
-            }
-        }
-        void emulateCycle();
-        void loadRom(const char* filename){
-            std::ifstream file(filename, std::ios::binary | std::ios::ate);
-            if (file.is_open()){
-                std::streampos size = file.tellg();
-                char* buffer = new char[size];
+chipeu::chipeu() : pc (0x200){
+    // Loading the fonstet in to the memory
+    for (int i = 0; i < 80; ++i){
+        memory[i] = chip8_fontset[i];
+    }
+}
 
-                file.seekg(0, std::ios::beg);
-                file.read(buffer, size);
-                file.close();
-                if (size<=0xE00){
-                    for (long i = 0; i < size; ++i){
-                        memory[0x200 + i ] = buffer[i];
-                    } 
+void chipeu::emulateCycle(){
+    opcode = memory[pc] << 8 | memory[pc+1];
+    switch(opcode & 0xF000){
+        {
+            case 0x0000:
+                switch(opcode & 0x000F){
+                    case 0x0000:
+                        for(int i = 0; i < 2048; ++i){
+                            gfx[i] = 0;
+                        }
+                    case 0x000E:
+                        --sp;
+                    default:
+                        std::cout << "Unknown opcode [0x" << std::hex << opcode << "]" << std::endl;
                 }
-                delete[] buffer;
-            }
         }
-    private:
-        // chip8_fontset each character is 5 bytes
-        static constexpr unsigned char chip8_fontset[80] = { 
-            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-            0x20, 0x60, 0x20, 0x20, 0x70, // 1
-            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-            0xF0, 0x80, 0xF0, 0x80, 0x80  // F
-        };
-        // 2 bytes for an opcode
-        unsigned short opcode{};
-        // 4 KiB of memory
-        unsigned char memory[4096]{};
-        // 16 CPU registers, 15 general purpose, 1 for carry flag
-        unsigned char V[16]{};
-        // Index Reigster
-        unsigned short I{};
-        // Program counter
-        unsigned short pc{};
-        // Display, sprites are XOR'ed on the screen
-        unsigned char gfx[64 * 32]{};
-        // Interuppts and hardware registers
-        unsigned char delay_timer{};
-        unsigned char sound_timer{};
-        // Stack
-        unsigned short stack[16]{};
-        unsigned short sp{};
-        // Input
-        unsigned char key[16]{};
-        bool drawflag {true};
+    }
 };
+
+void chipeu::loadRom(const char* filename){
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+    if (file.is_open()){
+        std::streampos size = file.tellg();
+        char* buffer = new char[size];
+
+        file.seekg(0, std::ios::beg);
+        file.read(buffer, size);
+        file.close();
+        if (size<=0xE00){
+            for (long i = 0; i < size; ++i){
+                memory[0x200 + i ] = buffer[i];
+            } 
+        }
+        delete[] buffer;
+    }
+}
